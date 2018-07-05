628a137b22993c630f8aef98cf555e5047843aff
User <zemlak@list.ru>
2018-07-05 13:09:02 +0300

    reorganize functional to make functions more universal

--- coursera.py
+++ coursera.py
@@ -16,49 +16,64 @@ def get_list_of_cources_urls(
     return [xml_tag.get_text() for xml_tag in feed_soup.findAll('loc')]
 
 
-def parse_web_page(web_page):
-    attr_mapping =  {
-        'Name': {'class_': 'title display-3-text'},
-        'Language': {'class_': 'rc-Language'},
-        'Nearest start date': {'class_': 'rc-StartDateString'},
-        'Raiting': {'class_': 'ratings-text'},
-        'Number of weeks': {'class_': 'week-heading',
-                            'type_': 'count_elements'}
-    }
-
+def parse_web_page(web_page, metadata):
     feed_soup = BeautifulSoup(web_page, 'html.parser')
 
-    parced_data = []
-    for _, attr_params in attr_mapping.items():
-        if not attr_params.get('type_'):
-            param_value = feed_soup.find(class_=attr_params.get('class_'))
+    data = {}
+    for param_name, param_preperties in metadata.items():
+        if not param_preperties.get('type_'):
+            param_value = feed_soup.find(class_=param_preperties.get('class_'))
             if isinstance(param_value, bs4.element.Tag):
-                parced_data.append(param_value.get_text())
+                data[param_name] = param_value.get_text()
             else:
-                parced_data.append(None)
+                data[param_name] = None
 
-        elif attr_params.get('type_') == 'count_elements':
-            param_value = feed_soup.find_all(class_=attr_params.get('class_'))
-            if isinstance(param_value, list):
-                parced_data.append(len(param_value))
-            else:
-                parced_data.append(None)
-
-    return parced_data
-
-
-def combine_array_with_cources_data(cources_urls):
-    result_array = [[course_name for course_name in cources_attr_mapping]]
-    for url in cources_urls:
-
-        response = requests.get(url)
-        if response.ok:
-            response.encoding = 'utf-8'
-            result_array.append(
-                parse_web_page(response.text, cources_attr_mapping)
+        elif param_preperties.get('type_') == 'count_elements':
+            param_value = feed_soup.find_all(
+                class_=param_preperties.get('class_')
             )
+            if isinstance(param_value, list):
+                data[param_name] = len(param_value)
+            else:
+                data[param_name] = None
 
-    return result_array
+    return data
+
+
+def get_cource_web_page(url):
+    response = requests.get(url)
+    if response.ok:
+        response.encoding = 'utf-8'
+        return response.text
+
+
+def make_cources_data_dict(cources_urls):
+    cources_metadata = {
+        'Name': {'class_': 'title display-3-text', 'order': 0},
+        'Language': {'class_': 'rc-Language', 'order': 1},
+        'Nearest start date': {'class_': 'rc-StartDateString', 'order': 2},
+        'Raiting': {'class_': 'ratings-text', 'order': 3},
+        'Number of weeks': {'class_': 'week-heading',
+                            'type_': 'count_elements', 'order': 4
+                            }
+    }
+
+    cources_data = {
+        'metadata': {'attributes_names': sorted(
+                cources_metadata.keys(),
+                key=lambda key: cources_metadata.get(key).get('order', 100))
+        },
+        'data': []
+    }
+    for url in cources_urls:
+        cources_data['data'].append(
+            parse_web_page(
+                web_page=get_cource_web_page(url),
+                metadata=cources_metadata
+            )
+        )
+
+    return cources_data
 
 
 def set_auto_width_excel_cols(worksheet, indent=4):
@@ -69,16 +84,36 @@ def set_auto_width_excel_cols(worksheet, indent=4):
         )
 
 
-def make_excel_workbook_from_array(array):
+def make_excel_workbook_from_table(table):
     work_book = Workbook()
     work_sheet = work_book.active
 
-    for row in array:
+    for row in table:
         work_sheet.append(row)
 
     set_auto_width_excel_cols(work_sheet)
 
     return work_book
+
+
+def make_table_from_data_dict(dict_):
+    '''input dictionary must contain key "data" where contains
+        a list of dictionaries with data. Also it could contain key "metadata"
+        and funtion tries to exctract column names from it'''
+
+    columns_names = (
+            dict_.get('metadata', {}).get('attributes_names') or
+            list(dict_.get('data', [{}])[0].keys())
+    )
+    table = [dict_.get('metadata', {}).get('attributes_names')]
+
+    for record in dict_.get('data'):
+        row = []
+        for attribute_name in columns_names:
+            row.append(record.get(attribute_name))
+        table.append(row)
+
+    return table
 
 
 def parse_arguments():
@@ -115,13 +150,15 @@ def validate_path_to_save_file(filepath):
 if __name__ == '__main__':
     params = parse_arguments()
 
-    rnd_cources_ulr_list = random.sample(
+    random_cources_ulr_list = random.sample(
         get_list_of_cources_urls(),
         k=params.cources_number
     )
 
-    excel_workbook = make_excel_workbook_from_array(
-        array=combine_array_with_cources_data(rnd_cources_ulr_list)
+    excel_workbook = make_excel_workbook_from_table(
+        table=make_table_from_data_dict(
+            make_cources_data_dict(random_cources_ulr_list)
+        )
     )
 
     excel_workbook.save(params.filepath)
